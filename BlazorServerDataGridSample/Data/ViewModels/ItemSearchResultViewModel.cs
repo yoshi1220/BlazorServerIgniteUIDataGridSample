7dbb45494140bbcb0febbdad5e797dc1d0af4969
yoshi1220 <19341602+yoshi1220@users.noreply.github.com>
2022-12-01 14:07:07 +0900

    サンプル検索ダイアログの作成

---
+++ BlazorServerDataGridSample/Data/ViewModels/ItemSearchResultViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorServerDataGridSample.Data.ViewModels
+{
+    public class ItemSearchResultViewModel
+    {
+        public int RowIndex { get; set; }
+
+        public string? ItemCode { get; set; }
+
+        public string? ItemName { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+    }
+}
